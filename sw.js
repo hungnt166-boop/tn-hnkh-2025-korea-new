1441dd43c728b187c09e996a15065c4768038247
hungnt166-boop <hungnt166@gmail.com>
2025-11-10 02:51:30 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,8 @@
-const CACHE = 'tn-hnkh-2025-korea-v8';
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith((async()=>{
-    const cache = await caches.open(CACHE);
-    const hit = await cache.match(e.request);
-    if(hit) return hit;
-    try{
-      const net = await fetch(e.request);
-      cache.put(e.request, net.clone());
-      return net;
-    }catch(err){
-      return cache.match('./index.html');
-    }
-  })());
+const CACHE='tn-hnkh-2025-korea-v10';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;
+  e.respondWith((async()=>{const cache=await caches.open(CACHE);const hit=await cache.match(e.request);
+    if(hit)return hit;try{const net=await fetch(e.request);cache.put(e.request,net.clone());return net;}catch(err){return cache.match('./index.html');}})());
 });
