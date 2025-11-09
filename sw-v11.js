5958c9b156386fd11e9308d5e4f02cd2fa899324
hungnt166-boop <hungnt166@gmail.com>
2025-11-10 03:11:59 +0700

    Add files via upload

---
+++ sw-v11.js
@@ -0,0 +1,8 @@
+const CACHE='tn-hnkh-2025-v11';
+const ASSETS=['./','./index.html','./manifest.webmanifest?v=11','./icon-192.png?v=11','./icon-512.png?v=11'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;
+  e.respondWith((async()=>{const cache=await caches.open(CACHE);const hit=await cache.match(e.request);
+    if(hit)return hit;try{const net=await fetch(e.request);cache.put(e.request,net.clone());return net;}catch(err){return cache.match('./index.html');}})());
+});
